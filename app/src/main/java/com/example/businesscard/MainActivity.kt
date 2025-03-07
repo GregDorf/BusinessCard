ba4c6f55f6d093b6cf2e0fd7f2b2dba0db363041
Greg Dorf <gregdorf86@gmail.com>
2025-03-07 19:59:43 +0400

    <style>(): Переделан весь интерфейс, добавлен градиент и прочая прочая прочая

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -40,7 +40,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
-// main
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
